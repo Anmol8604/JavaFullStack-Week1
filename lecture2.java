463c021b800e4546fc4153417ecfe439c7483551
Anmol8604 <anmol.12018604@lpu.in>
2023-06-23 02:29:39 +0530

    lec2

--- lecture2.java
+++ lecture2.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+/**
 public class lecture2 {
 	 public static void main(String[] args) {
 		System.out.println("Enter any number: ");
@@ -15,4 +16,23 @@ public class lecture2 {
 		}
 	 }
 }
+**/
 
+public class lecture2 {
+	 public static void main(String[] args) {
+		  System.out.println("Enter Basic Pay: ");
+		  Scanner sc = new Scanner(System.in);
+		  double basicpay = sc.nextInt();
+		  double hra = 0, da = 0;
+		  if(basicpay >= 5000) {
+			   hra = 0.15 * basicpay;
+			   da = 0.25 * basicpay;
+		  }
+		  else {
+			   hra = 0.2 * basicpay;
+			   da = 0.3 * basicpay;
+		  }
+		  System.out.println(hra + " " + da);
+		  System.out.println(hra + da + basicpay);
+	 }
+}
